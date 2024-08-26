1d4fda21efc2609e65204f272db63f5800b8ebf7
Gokul Priyan <gokulpriyan781@gmail.com>
2024-08-26 09:37:40 +0530

    1

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -1,2 +1,27 @@
-public class TwoSum {
+import java.util.HashMap;
+
+public class TwoSum
+{
+    public int[] two(int[] nums, int target)
+    {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            int c = target-nums[i];
+            if(map.containsKey(c))
+            {
+                return new int[]{map.get(c),i};
+            }
+            map.put(nums[i],i);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int t = 9;
+        TwoSum ta = new TwoSum();
+        int[] aa =ta.two(nums,t);
+        System.out.println(aa[0]+""+aa[1]) ;
+    }
 }
