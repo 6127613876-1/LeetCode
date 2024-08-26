d20a323de86ceb4c51b92b8f3154a58a59f4f7bb
Gokul Priyan <gokulpriyan781@gmail.com>
2024-08-26 10:23:49 +0530

    1

---
+++ src/BuyandSellStock.java
@@ -0,0 +1,30 @@
+public class BuyandSellStock
+{
+    public int max(int[] prices)
+    {
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+        for(int i=0;i<prices.length;i++)
+        {
+            if(prices[i]<min)
+            {
+                min = prices[i];
+            }
+            else
+            {
+                int p = prices[i]-min;
+                if(p>max)
+                {
+                    max = p;
+                }
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        BuyandSellStock bs = new BuyandSellStock();
+        int[] p = {7,1,5,3,6,4};
+        System.out.println(bs.max(p));
+    }
+}
